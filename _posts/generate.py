3efbfcabb031ef45e9c274c2dc6a3c31c5a69370
p-i-c-o <95228665+p-i-c-o@users.noreply.github.com>
2023-07-10 22:37:22 -0700

    commit8

---
+++ _posts/generate.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+def get_current_date():
+    current_date = datetime.now().strftime('%Y-%m-%d')
+    return current_date
+
+def create(filename, content):
+    with open(filename, 'w+') as f:
+	     f.write(content)
+	     f.close()
+
+date = get_current_date()
+title = input('Article title >>> ')
+tags =  input('Article tags  >>> ').split(' ')
+
+template = f"""title: {title}
+date: {date} +/-TTTT
+tags: {tags}
+author: me
+"""
+
+create(date+"-"+title.replace(' ', '-'), template)
